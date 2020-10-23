3c0da7d9eb1c4049e8c96e1ee1edc6a8ebed3697
Thomas Bonk <thomas@meandmymac.de>
2020-10-23 13:28:24 +0200

    Project creation

---
+++ README.md
@@ -0,0 +1,4 @@
+# Repti
+
+Repti is an application for managing data of the reptiles that
+you are keeping.
---
+++ android/app/src/main/kotlin/com/example/repti/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.repti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
