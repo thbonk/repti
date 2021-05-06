f7f6efc210d9ab5dd6a09968d39a11c868691e17
Thomas Bonk <thomas@meandmymac.de>
2021-05-06 07:02:12 +0200

    starting development on document subview

---
+++ Repti/Source/UI/Views/IndividualDetails/DocumentsSubview.swift
@@ -0,0 +1,33 @@
+//
+//  DocumentsSubview.swift
+//  Repti
+//
+//  Created by Thomas Bonk on 06.05.21.
+//  Copyright 2021 Thomas Bonk <thomas@meandmymac.de>
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import SwiftUI
+
+struct DocumentsSubview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DocumentsSubview_Previews: PreviewProvider {
+    static var previews: some View {
+        DocumentsSubview()
+    }
+}
